04f9e4593e9d687acf5991a2ddc4f74dc087a603
Shiryu Valecillos Personal <shiryuevfr@gmail.com>
2023-11-20 19:12:21 -0300

    cambios

--- frontWeb/src/app/Administracion/administracion/productos/productos.service.ts
+++ frontWeb/src/app/Administracion/administracion/productos/productos.service.ts
@@ -7,13 +7,13 @@ import { Producto } from './producto.model'; // crear un modelo para Producto
   providedIn: 'root'
 })
 export class ProductosService {
-  private apiUrl = 'https://forcemental.azure-api.net/productos/api/productos/'; // Reemplaza con la URL de tu API real
+  private apiUrl = 'https://forcemental.azure-api.net/productos'; // Reemplaza con la URL de tu API real
 
   constructor(private http: HttpClient) {}
 
   // Obtener todos los productos
   getProductos(): Observable<Producto[]> {
-    return this.http.get<Producto[]>(this.apiUrl);
+    return this.http.get<Producto[]>(`${this.apiUrl}/api/productos/`);
   }
 
   // Obtener un producto por ID
@@ -29,17 +29,17 @@ export class ProductosService {
       })
     };
 
-    return this.http.post<Producto>('http://107.22.174.168:8020/api/productos/', producto, httpOptions);
+    return this.http.post<Producto>(`${this.apiUrl}/api/productos/`, producto, httpOptions);
   }
 
   // Actualizar un producto
   updateProducto(producto: Producto): Observable<Producto> {
     console.log(producto.id_producto);
-    return this.http.put<Producto>(`${'http://107.22.174.168:8020/api/productos/'}${producto.id_producto}/`, producto);
+    return this.http.put<Producto>(`${this.apiUrl}/api/productos/${producto.id_producto}/`, producto);
   }
 
   // Eliminar un producto
   deleteProducto(id: number): Observable<Producto> {
-    return this.http.delete<Producto>(`${'http://107.22.174.168:8020/api/productos/'}${id}`);
+    return this.http.delete<Producto>(`${this.apiUrl}/api/productos/${id}`);
   }
 }
